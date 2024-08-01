edfa6c8e472c6f68ac934c9476e959ff5af16728
GP-CoderEpic <prashargaurav20@gmail.com>
2024-08-01 22:15:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const User = require('./models/user');
+const Blog = require('./models/blog');
+
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then((e) => console.log("MongoDB connectet"));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find({}).sort("createdAt");
+    // const name = await User.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
+
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
+app.listen(PORT,() => console.log(`Server Started at PORT: ${PORT}`));
